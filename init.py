9a3e8e49f0200eb1290a62eae9545aa8c38d3635
Rajat Yadav <rajatyadav05.ry@gmail.com>
2019-03-14 21:55:49 +0530

    changes

--- init.py
+++ init.py
@@ -1,1 +1,10 @@
-form flask import Flask
+from flask import Flask,render_template
+
+app=Flask(__name__)
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+
+if __name__=="__main__":
+	app.run(debug="true",host="0.0.0.0")
